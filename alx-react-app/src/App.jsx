39c20213a3f6097eebb0051991a94f71cc7c8217
hassanfahmyzaky <hassanfahmyzaky@gmail.com>
2025-02-23 21:20:36 +0200

    3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
@@ -10,6 +13,9 @@ function App() {
   return (
     <>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
       
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -19,9 +25,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      
+
       <h1>Vite + React</h1>
-      
+
       <div className="card">
         <button onClick={() => setCount(count + 1)}>
           count is {count}
@@ -30,7 +36,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
