dbad39e4a121032334251fd774290571d21d71d1
hassanfahmyzaky <hassanfahmyzaky@gmail.com>
2025-02-23 21:47:52 +0200

    7++++

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,10 +1,8 @@
-// UserContext.js
-
+// src/components/UserContext.js
 import { createContext } from 'react';
 
-// Step 1: Initialize the context with a default value
-// The default value is an object with name and email
-export const UserContext = createContext({
-  name: "Default User",
-  email: "default@example.com"
-});
+// Create the Context for user data
+const UserContext = createContext();
+
+// Export it as default
+export default UserContext;
