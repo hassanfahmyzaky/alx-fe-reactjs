98e226ec6ad63ef7bc54c84b6482c7bef3c873a9
hassanfahmyzaky <hassanfahmyzaky@gmail.com>
2025-03-09 23:53:54 +0200

    11.2

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,17 +1,39 @@
 import create from 'zustand';
 
 const useRecipeStore = create(set => ({
-  recipes: [],
-  searchTerm: '',
-  filteredRecipes: [],
+  recipes: [], // List of recipes
+  searchTerm: '', // Search term for filtering
+  filteredRecipes: [], // Filtered recipes based on the search term
+
+  // Action to set the search term
   setSearchTerm: (term) => set({ searchTerm: term }),
+
+  // Action to filter recipes based on the search term
   filterRecipes: () => set(state => ({
     filteredRecipes: state.recipes.filter(recipe =>
       recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
     )
   })),
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+
+  // Action to add a new recipe to the store
+  addRecipe: (newRecipe) => set(state => ({
+    recipes: [...state.recipes, newRecipe]
+  })),
+
+  // Action to set initial recipes
+  setRecipes: (recipes) => set({ recipes }),
+
+  // Action to update an existing recipe
+  updateRecipe: (updatedRecipe) => set(state => ({
+    recipes: state.recipes.map(recipe =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    )
+  })),
+
+  // Action to delete a recipe by id
+  deleteRecipe: (id) => set(state => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== id)
+  })),
 }));
 
 export { useRecipeStore };
