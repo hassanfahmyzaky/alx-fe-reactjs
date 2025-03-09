ed719c92cdf8a8acd0ead8824a86a7d67ec75a11
hassanfahmyzaky <hassanfahmyzaky@gmail.com>
2025-03-09 23:58:18 +0200

    Just Playing

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -28,3 +28,30 @@ const RecipeList = () => {
 };
 
 export default RecipeList;
+
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from './recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore(state => state.recipes);
+
+  return (
+    <div>
+      {recipes.length === 0 ? (
+        <p>No recipes available. Add some!</p>
+      ) : (
+        recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+            <Link to={`/recipe/${recipe.id}`}>
+              <button>View Recipe Details</button>
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default RecipeList;
