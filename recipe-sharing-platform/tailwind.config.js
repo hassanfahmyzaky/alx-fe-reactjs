fc8c63bf783bacb8886c4a9bb03a3b2ab47e94d4
hassanfahmyzaky <hassanfahmyzaky@gmail.com>
2025-03-23 00:12:43 +0200

    recipe sharing platform

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // Optional: change to 'media' or 'class' if you want dark mode support
+    theme: {
+      extend: {}, // Customize your theme here (e.g., colors, spacing)
+    },
+    plugins: [], // Add any Tailwind plugins here (if needed)
+  };
+  
